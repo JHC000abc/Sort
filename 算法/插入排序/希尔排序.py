7c8a703a9837ec02e081b3a553de31a240bcc1cb
JHC <3237558741@qq.com>
2021-09-20 14:17:27 +0800

    希尔

--- 算法/插入排序/希尔排序.py
+++ 算法/插入排序/希尔排序.py
@@ -1,25 +1,16 @@
-def ShellSort(lst):
-    def shellinsert(arr,d):
-        n=len(arr)
-        for i in range(d,n):
-            j=i-d
-            temp=arr[i]             #记录要出入的数
-            while(j>=0 and arr[j]>temp):    #从后向前，找打比其小的数的位置
-                arr[j+d]=arr[j]                 #向后挪动
-                j-=d
-            if j!=i-d:
-                arr[j+d]=temp
-    n=len(lst)
-    if n<=1:
-        return lst
-    d=n//2
-    while d>=1:
-        shellinsert(lst,d)
-        d=d//2
-    return lst
+def shell_Sort(arr):
+   target=len(arr)//2
+   while target>0:
+       for j in range(target,len(arr)):
+           i=j
+           while i>0 and arr[i]<arr[i-target]:
+               arr[i],arr[i-target]=arr[i-target],arr[i]
+               i-=target
+       target//=2
 
 
 if __name__ == '__main__':
-    arr = [9, 8, 7, 0, 6, 5, 1, 4, 3, 2]
-    ShellSort(arr)
+    arr = [9, 8, 7, 0, 6,5, 1, 4, 3, 2]
+    shell_Sort(arr)
     print(arr)
+
